bbbd4b73ba373c26ae83ea6de25a915cb8691cba
Your Name <you@example.com>
2022-10-12 09:36:52 +0200

    simple CRUD

--- app/main.py
+++ app/main.py
@@ -1,15 +1,44 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+paczki = {}
+
+class Paczka(BaseModel):
+    id: str
+    paczkomat_docelowy: str
+    opis: Union[str, None] = None
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"jazda": "jazda"}
+async def app_root():
+    return {"paczkomaty": "impost"}
 
+@app.get("/regulamin")
+async def app_root():
+    return "nie wolno umieszać rzeczy w skrytkach przez klientów odbierających paczki"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/paczki")
+async def get_paczki():
+    return paczki
+
+@app.get("/paczka")
+async def get_paczka(id: str):
+    if id in paczki:
+        return paczki[id]
+    return f'brak paczki o id: {id}'
+
+@app.post("/paczka")
+async def post_paczki(paczka: Paczka):
+    paczki[paczka.id] = paczka
+    return paczka
+
+@app.delete("/paczki")
+async def delete_paczki(id: str):
+    if id in paczki:
+        paczki.pop(id)
+        return f'usunieto paczkę o id: {id}'
+    return f'brak paczki o id: {id}'
