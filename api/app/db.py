6087b692b1fe9839656d8449029584aeb1c2a62c
Patryk Guba <gubapatryk@gmail.com>
2022-10-26 09:27:08 +0200

    modify ports

--- api/app/db.py
+++ api/app/db.py
@@ -1,5 +1,5 @@
 from sqlmodel import create_engine
 
-conn_str = "postgresql://izydor:test@localhost:5444/bm_dev_auth"
+conn_str = "postgresql://izydor:test@localhost:7777/bm_dev_auth"
 
 engine = create_engine(conn_str, echo=True)
